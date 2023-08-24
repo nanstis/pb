f0411e5df7fd5dac29e1bc7bb039c88a05926f44
nanstis <bleyo@alphomega.org>
2023-08-24 15:00:15 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        "./resources/js/*.js",
+        "./resources/css/*.css",
+    ],
+    theme: {
+        extend: {
+            extend: {
+                animation: ['group-hover'],
+                colors: {
+                    'primary': '#fe8a02',
+                    'plan-standard': '#b1d236',
+                    'plan-premium': '#ffc64b',
+                    'plan-exclusive': '#fe8a02',
+                    'plan-vip': '#85338b',
+                },
+                fontFamily: {
+                    'body': ['"Libre Baskerville"'],
+                },
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/container-queries'),
+    ],
+    safelist: [
+        'bg-plan-standard',
+        'bg-plan-premium',
+        'bg-plan-exclusive',
+        'bg-plan-vip',
+        'text-plan-standard',
+        'text-plan-premium',
+        'text-plan-exclusive',
+        'text-plan-vip'
+    ]
+};
